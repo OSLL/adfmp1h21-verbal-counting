8732aa5b0024f99063abb9f9db93987ed9db84bf
Alexander <chernokoz@hotmail.com>
2021-04-14 05:53:38 +0300

    init android project, add mockup and use cases

---
+++ app/src/main/java/com/chernokoz/android/verbalcounting/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.chernokoz.android.verbalcounting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
